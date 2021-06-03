99322475b63fa23093d2c24ac13cff232bd3066f
Brinton <brintonfoyreed@gmail.com>
2021-06-03 16:45:34 -0700

    commit tree 4511b75432e3bd6c6f9454f9a13811a13a95e35e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+# Write your code here. 
+
+def badge_maker(name)
+    p "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name| 
+       "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(names)
+    counter = 0
+    names.map do |name|
+        counter +=1
+    "Hello, #{name}! You'll be assigned to room #{counter}!"
+    end
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |name|
+        puts name
+    end
+    assign_rooms(names).each do |name|
+        puts name
+    end
+end
